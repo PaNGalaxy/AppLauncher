8520f481b820f86134593e261ce729d9a40c2413
Gregory Cage <cagege@ornl.gov>
2024-05-01 17:24:05 -0400

    Remove old template app code

--- template_app/app/app_launcher_panel.py
+++
@@ -1,8 +0,0 @@
-from trame.widgets import vuetify3 as vuetify
-
-
-class AppLauncherPanel:
-
-	def __init__(self, input_fields):
-		with vuetify.VCol(cols="12", sm="6", md="3" if two_col else "6"):
-                        vuetify.VLabel("Launch apps from here", density="compact")
--- template_app/app/login_view.py
+++
@@ -1,9 +0,0 @@
-from trame.widgets import vuetify3 as vuetify
-
-from .auth import TrameAuth as auth
-
-class LoginView:
-
-    def __init__(self):
-        with vuetify.VRow(align="center"):
-            vuetify.VBtn("Login", id="login-button", href=auth.get_auth_url(), flat=True)
