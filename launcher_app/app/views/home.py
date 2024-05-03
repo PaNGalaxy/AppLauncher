0704a641c925fda325b201908664351431559917
Gregory Cage <cagege@ornl.gov>
2024-05-03 16:20:41 -0400

    Add ability to stop running topaz app

--- launcher_app/app/views/home.py
+++ launcher_app/app/views/home.py
@@ -1,3 +1,6 @@
+import re
+from functools import partial
+
 from trame.app import asynchronous
 from trame.widgets import client
 from trame.widgets import vuetify3 as vuetify
@@ -13,6 +16,7 @@ class HomeView:
         self.ctrl = self.server.controller
         self.job_vm = job_view_model
         self.job_vm.job_state_bind.connect("job_state")
+        self.job_vm.jobs_bind.connect("jobs")
         self.user_vm = user_view_model
         self.js_navigate = client.JSEval(
                 exec="window.open($event,'_blank')"
@@ -24,8 +28,23 @@ class HomeView:
 
     def create_ui(self):
         with vuetify.VRow(align="center"):
-            vuetify.VBtn("Start Topaz Reduction tool", id="login-button-xcams", click=self.invoke_topaz_tool, style="padding: 10px; margin: 10px;")
-            vuetify.VProgressCircular(id="launch_tool_progress", color="red", indeterminate=True, v_show="job_state =='launching'")
+            vuetify.VBtn(
+                "Start Topaz Reduction tool",
+                id="start-topaz-btn",
+                click=self.invoke_topaz_tool,
+                style="padding: 10px; margin: 10px;",
+            )
+            vuetify.VProgressCircular(
+                id="launch_tool_progress",
+                color="red",
+                indeterminate=True,
+                v_show="job_state =='launching'"
+            )
+        with vuetify.VRow(align="center", v_for="(job_info, job) in jobs"):
+            vuetify.VTextField(" Job ID: {{ job }}")
+            vuetify.VTextField("{{ job_info.tool_id }}")
+            vuetify.VBtn("Go to Tool", click=partial(self.navigate_to_topaz_tool, ""), style="margin: 10px;")
+            vuetify.VBtn("Stop", click=partial(self.stop_topaz_job, ""), style="margin: 10px;")
 
     @asynchronous.task
     async def invoke_topaz_tool(self):
@@ -33,3 +52,15 @@ class HomeView:
         if current_url is not None:
             self.js_navigate(current_url)
 
+    def navigate_to_topaz_tool(self, job_url):
+        for job, info in self.job_vm.jobs.items():
+            if info["tool_id"] == "neutrons_trame_topaz":
+                url = info["url"]
+                self.js_navigate(url)
+
+    @asynchronous.task
+    async def stop_topaz_job(self, job_id):
+        for job, info in self.job_vm.jobs.items():
+            if info["tool_id"] == "neutrons_trame_topaz":
+                await self.job_vm.stop_job(job)
+                break
