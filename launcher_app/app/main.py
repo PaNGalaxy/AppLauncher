87a819d560d6f20aecf0a47d732557417e181762
Gregory Cage <cagege@ornl.gov>
2024-05-20 16:54:28 -0400

    Implement passing a path instead of /redirect via env var

--- launcher_app/app/main.py
+++ launcher_app/app/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from launcher_app.app.views.main import App
@@ -5,7 +6,8 @@ from launcher_app.app.utilities.auth import TrameAuth
 
 
 def main(server=None, **kwargs):
-    TrameAuth.start_session("/redirect")
+    redirect_path = os.getenv("TRAME_REDIRECT_PATH", "/redirect")
+    TrameAuth.start_session(redirect_path)
     app = App(server)
     for arg in sys.argv[1:]:
         try:
