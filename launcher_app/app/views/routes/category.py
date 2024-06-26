041617ea214593f2fdaeb8a8ab079a4f46267d37
John Duggan <dugganjw@ornl.gov>
2024-06-26 15:42:29 -0400

    Make list striping opt-in rather than opt-out

--- launcher_app/app/views/routes/category.py
+++ launcher_app/app/views/routes/category.py
@@ -58,7 +58,7 @@ class CategoryView:
                     )
                 )
                 with vuetify.VCardText():
-                    with vuetify.VList():
+                    with vuetify.VList(classes="striped"):
                         vuetify.VListSubheader(
                             "Available Tools",
                             v_if=("tools[$route.params.category]['tools'].length > 0",),
